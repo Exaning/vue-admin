c55a4801fae3075dfe03f699ceba73b68543048c
Exan <ExanNur@163.com>
2021-08-12 17:14:00 +0800

    homeView-de 的 新代码

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -58,7 +58,7 @@
             </el-menu-item>
           </el-menu>
         </el-aside>
-        <el-main>Main</el-main>
+        <el-main>MainPanel</el-main>
       </el-container>
     </el-container>
   </div>
